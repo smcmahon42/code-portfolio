7f157bca6c0d362eac248c39c5fc5573628de622
Sean McMahon <sean@orangemco.com>
2014-07-29 21:08:38 -0700

    pare down more code in mindspace site and removed more videos from hyatt-chase

--- mindspace site/code/sql/guests.sql
+++
@@ -1,9 +0,0 @@
-SET FOREIGN_KEY_CHECKS=0;
-CREATE TABLE guests (
-    guest_id int(11) NOT NULL auto_increment,
-    username varchar(100) default NULL,
-    password varchar(100) default NULL,
-    points int(11) default 0,
-    tasks text default null,
-    PRIMARY KEY  (guest_id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
